484dc8f1c75e690dcefcba40154ba9e4e51fd13f
TemaSaur <69428015+TemaSaur@users.noreply.github.com>
2021-03-28 19:43:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-console.log(`   =============
-  |             |
-  |  bork bork  |
-  |             |
-   =============
-         \\/
-
+console.log(`    =============
+   |             |
+   |  bork bork  |
+   |             |
+    =============
+          V
   8Q               8#
   gr=xd#  #####  #5v=k
   E^..!v}])rrr)]i/!..1
@@ -15,15 +14,15 @@ console.log(`   =============
   U'   *iix7}]xi7=   !Q   1'-/vvvxxvv\,~Q
   Q=   '_-cggg|_-'   2    gx|]]jQ##2]]>:E
    8*.'/i==yWu:~u*'-;jR&g8gDfykWNEQ3}u~v#
-    B\!"Y8bxr}g&\:!;;~~;;>;~~~~~~;^rYi}B
+    B\\!"Y8bxr}g&\\:!;;~~;;>;~~~~~~;^rYi}B
     #x-_:!~)x|=!:-.=;;;;;;;;;;;;;;;;;}#
      k~_'''''''''-=^^^^^^^^^^^^^^^^^^*R
      g/*~".'''.,=********************rs
       dv))|***|r|||||r;=^))))))))))))/5
        y/xx=::>*~~~~*~=xzdEK/vxxxxxxxxE
        gr^rrrr;)^^^>v8#    #1>*v}1l11}&
-        I***f #R\)))K       Qv**]zlyy1R
-       #EvvvE #Ex]]x&       #z\)n#&yccH
+        I***f #R\\)))K       Qv**]zlyy1R
+       #EvvvE #Ex]]x&       #z\\)n#&yccH
       d}}}}}#8nu11uc#     #yi}}ikNclccc#
-      Q&&&&8 BRRRRRQ       &&&&&QQ&&&&Q\n
+      Q&&&&8 BRRRRRQ       &&&&&QQ&&&&Q
 `)
