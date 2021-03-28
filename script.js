67dd79b582a48bed457b03ccf7c28fb9ce8edd51
TemaSaur <69428015+TemaSaur@users.noreply.github.com>
2021-03-28 19:41:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-console.log(`
-  8Q               8#                    
-  gr=xd#  #####  #5v=k                    
-  E^..!v}])rrr)]i/!..1                    
-  &r_:^rrrrrrrrrrr>""y                    
-  Qv\\/////\\/////\\/)//f      QAfy}yWg#     
-  &xxxxIRyvvvxvjRkvxxy     3*_.._,--!yB   
-  j-._*RBax]]]xbBA;-.~Q   d!._;r|r|r~_~R  
-  U'   *iix7}]xi7=   !Q   1'-/vvvxxvv\,~Q 
-  Q=   '_-cggg|_-'   2    gx|]]jQ##2]]>:E 
-   8*.'/i==yWu:~u*'-;jR&g8gDfykWNEQ3}u~v# 
-    B\\!"Y8bxr}g&\\:!;;~~;;>;~~~~~~;^rYi}B  
-    #x-_:!~)x|=!:-.=;;;;;;;;;;;;;;;;;}#   
-     k~_'''''''''-=^^^^^^^^^^^^^^^^^^*R   
-     g/*~".'''.,=********************rs   
-      dv))|***|r|||||r;=^))))))))))))/5   
-       y/xx=::>*~~~~*~=xzdEK/vxxxxxxxxE   
-       gr^rrrr;)^^^>v8#    #1>*v}1l11}&   
-        I***f #R\\)))K       Qv**]zlyy1R   
-       #EvvvE #Ex]]x&       #z\\)n#&yccH   
-      d}}}}}#8nu11uc#     #yi}}ikNclccc#  
-      Q&&&&8 BRRRRRQ       &&&&&QQ&&&&Q   
-`);
+console.log(`   =============
+  |             |
+  |  bork bork  |
+  |             |
+   =============
+         \\/
+
+  8Q               8#
+  gr=xd#  #####  #5v=k
+  E^..!v}])rrr)]i/!..1
+  &r_:^rrrrrrrrrrr>""y
+  Qv\\/////\\/////\\/)//f      QAfy}yWg#
+  &xxxxIRyvvvxvjRkvxxy     3*_.._,--!yB
+  j-._*RBax]]]xbBA;-.~Q   d!._;r|r|r~_~R
+  U'   *iix7}]xi7=   !Q   1'-/vvvxxvv\,~Q
+  Q=   '_-cggg|_-'   2    gx|]]jQ##2]]>:E
+   8*.'/i==yWu:~u*'-;jR&g8gDfykWNEQ3}u~v#
+    B\!"Y8bxr}g&\:!;;~~;;>;~~~~~~;^rYi}B
+    #x-_:!~)x|=!:-.=;;;;;;;;;;;;;;;;;}#
+     k~_'''''''''-=^^^^^^^^^^^^^^^^^^*R
+     g/*~".'''.,=********************rs
+      dv))|***|r|||||r;=^))))))))))))/5
+       y/xx=::>*~~~~*~=xzdEK/vxxxxxxxxE
+       gr^rrrr;)^^^>v8#    #1>*v}1l11}&
+        I***f #R\)))K       Qv**]zlyy1R
+       #EvvvE #Ex]]x&       #z\)n#&yccH
+      d}}}}}#8nu11uc#     #yi}}ikNclccc#
+      Q&&&&8 BRRRRRQ       &&&&&QQ&&&&Q\n
+`)
